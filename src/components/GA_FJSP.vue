10461bc81105aa1edd9039af30b5f4631aefd6f0
samwise-coder <974049754@qq.com>
2024-05-21 00:37:30 +0800

    feat:新增echarts图表

--- src/components/GA_FJSP.vue
+++ src/components/GA_FJSP.vue
@@ -19,8 +19,8 @@
 </template>
 
 <script setup>
-import * as echarts from 'echarts'
-import { ref,onMounted } from "vue";
+import * as echarts from "echarts";
+import { ref, onMounted } from "vue";
 import { Mk01 } from "../assets/Mk01";
 import _ from "lodash";
 const tableConfig = ref([]);
@@ -28,218 +28,328 @@ const machineNum = Mk01[0][1];
 const tableData = ref([]);
 const population = 1; //种群数量
 
-
 const machines = getMachines(machineNum); // 所有机器
 tableConfig.value = getTableConfig(machineNum);
 tableData.value = originalDataVisualization(Mk01);
-console.log('tableData.value',tableData.value)
+console.log("tableData.value", tableData.value);
 const jobs = getJobs(Mk01); // 工件数组
 const To = getProcessNum(jobs); // 工序数
 
-console.log('To',To, "\njobs", jobs);
+console.log("To", To, "\njobs", jobs);
 
-onMounted(()=>{
-  initChart(machines)
-})
+onMounted(() => {
+  var chartDom = document.getElementById("gantt");
+  var myChart = echarts.init(chartDom);
+  option && myChart.setOption(option);
+});
 
-let MachinePartChromosomes = getMachinePartChromosomes(jobs,population,machines); // 机器部分染色体集
-let ProcessPartChromosomes = getProcessPartChromosomes(jobs,population,machines); // 工序部分染色体集
+let MachinePartChromosomes = getMachinePartChromosomes(
+  jobs,
+  population,
+  machines
+); // 机器部分染色体集
+let ProcessPartChromosomes = getProcessPartChromosomes(
+  jobs,
+  population,
+  machines
+); // 工序部分染色体集
 
-fitness(MachinePartChromosomes[0], ProcessPartChromosomes[0],jobs);
+fitness(MachinePartChromosomes[0], ProcessPartChromosomes[0], jobs);
 
 // cross(To);
-function initChart(yData) {
-  const gantt =document.getElementById('gantt')
-  const ganttChart = echarts.init(gantt)
-  const option = {
-    grid:{
-      show:false,
-      right:20,
-      left:80,
-      top:20,
-      bottom:20,
-      backgroundColor:'#fff'
-    },
-    legend:{
-      data:[''],
-      align:'auto',
-      top:'bottom'
-    },
-    xAxis:{
-      position:'bottom',
-      type:'time'
-    },
-    yAxis:{
-      data:yData
-    },
-    series:[{
-      type:'bar',
-      stack:'duration',
-      data:['2024-1-1','2024-2-3','2024-4-6','2024-4-20','2024-4-30','2024-5-6','2024-5-16']
+
+var option;
+
+var data = [];
+var dataCount = 10;
+var startTime = +new Date();
+var categories = ["categoryA", "categoryB", "categoryC"];
+var types = [
+  { name: "JS Heap", color: "#7b9ce1" },
+  { name: "Documents", color: "#bd6d6c" },
+  { name: "Nodes", color: "#75d874" },
+  { name: "Listeners", color: "#e0bc78" },
+  { name: "GPU Memory", color: "#dc77dc" },
+  { name: "GPU", color: "#72b362" },
+];
+// Generate mock data
+categories.forEach(function (category, index) {
+  var baseTime = startTime;
+  for (var i = 0; i < dataCount; i++) {
+    var typeItem = types[Math.round(Math.random() * (types.length - 1))];
+    var duration = Math.round(Math.random() * 10000);
+    data.push({
+      name: typeItem.name,
+      value: [index, baseTime, (baseTime += duration), duration],
+      itemStyle: {
+        normal: {
+          color: typeItem.color,
+        },
+      },
+    });
+    baseTime += Math.round(Math.random() * 2000);
+  }
+});
+function renderItem(params, api) {
+  var categoryIndex = api.value(0);
+  var start = api.coord([api.value(1), categoryIndex]);
+  var end = api.coord([api.value(2), categoryIndex]);
+  var height = api.size([0, 1])[1] * 0.6;
+  var rectShape = echarts.graphic.clipRectByRect(
+    {
+      x: start[0],
+      y: start[1] - height / 2,
+      width: end[0] - start[0],
+      height: height,
     },
     {
-      type:'bar',
-      stack:'duration',
-      data:['2023-1-1','2023-2-3','2023-4-6','2023-4-20','2023-4-30','2023-5-6','2023-5-16']
-    },]
-  }
-  ganttChart.setOption(option)
+      x: params.coordSys.x,
+      y: params.coordSys.y,
+      width: params.coordSys.width,
+      height: params.coordSys.height,
+    }
+  );
+  return (
+    rectShape && {
+      type: "rect",
+      transition: ["shape"],
+      shape: rectShape,
+      style: api.style(),
+    }
+  );
 }
+option = {
+  tooltip: {
+    formatter: function (params) {
+      return params.marker + params.name + ": " + params.value[3] + " ms";
+    },
+  },
+  title: {
+    text: "Profile",
+    left: "center",
+  },
+  dataZoom: [
+    {
+      type: "slider",
+      filterMode: "weakFilter",
+      showDataShadow: false,
+      top: 400,
+      labelFormatter: "",
+    },
+    {
+      type: "inside",
+      filterMode: "weakFilter",
+    },
+  ],
+  grid: {
+    height: 300,
+  },
+  xAxis: {
+    min: startTime,
+    scale: true,
+    axisLabel: {
+      formatter: function (val) {
+        return Math.max(0, val - startTime) + " ms";
+      },
+    },
+  },
+  yAxis: {
+    data: categories,
+  },
+  series: [
+    {
+      type: "custom",
+      renderItem: renderItem,
+      itemStyle: {
+        opacity: 0.8,
+      },
+      encode: {
+        x: [1, 2],
+        y: 0,
+      },
+      data: data,
+    },
+  ],
+};
 // 适应度函数 目标：最大完工时间最小化，f=min(max(Cj))
 // 计算每个个体的最大完工时间
-function fitness(mpc, ppc,xJobs) {
+function fitness(mpc, ppc, xJobs) {
   // 机器选择部分解码
   let Jm = []; //机器顺序矩阵
   let T = []; //时间顺序矩阵
-  console.log("mpc", mpc,'\nppc', ppc);
-  let i = 0
-  xJobs.forEach((ele)=>{
-    let _machRow=[]
-    let _timeRow=[]
-    ele.forEach(eleSon=>{
-      _machRow.push(eleSon['machineTimeMatrix'][mpc[i]-1][0] )
-      _timeRow.push(eleSon['machineTimeMatrix'][mpc[i]-1][1] )
-      i++
-    })
-    Jm.push(_machRow)
-    T.push(_timeRow)
-  })
-  console.log("Jm", Jm,'\nT',T);
+  console.log("mpc", mpc, "\nppc", ppc);
+  let i = 0;
+  xJobs.forEach((ele) => {
+    let _machRow = [];
+    let _timeRow = [];
+    ele.forEach((eleSon) => {
+      _machRow.push(eleSon["machineTimeMatrix"][mpc[i] - 1][0]);
+      _timeRow.push(eleSon["machineTimeMatrix"][mpc[i] - 1][1]);
+      i++;
+    });
+    Jm.push(_machRow);
+    T.push(_timeRow);
+  });
+  console.log("Jm", Jm, "\nT", T);
 
   // 工序排序部分解码
   // 转换成 O(jh) 第j个工件的第h道工序
   // 首先求解出工序索引(h)矩阵：相当于每个工件的工序的索引，和工序排序染色体中的基因一一对应
-  let _hasCal=[]
-  let hMatrix=[]
-  ppc.forEach(ele=>{
-    _hasCal.push(ele)
-    hMatrix.push(_hasCal.filter(gene=>gene===ele).length)
-  })
-  console.log('hMatrix',hMatrix)
-  // 
-  let Ojh = {} // 工序层面 工件j的第h道工序
-  let Mijh = {} // 机器层面  工件j的第h道工序在机器i上加工
-  machines.forEach(ele=>{
-    Mijh[ele] = []
-  })
-  ppc.forEach((ele,index)=>{
-    const machineKey = `machine${Jm[ele-1][hMatrix[index]-1]}`
-    const j = ele
-    const h = hMatrix[index]
-    const duration = T[ele-1][hMatrix[index]-1]
+  let _hasCal = [];
+  let hMatrix = [];
+  ppc.forEach((ele) => {
+    _hasCal.push(ele);
+    hMatrix.push(_hasCal.filter((gene) => gene === ele).length);
+  });
+  console.log("hMatrix", hMatrix);
+  //
+  let Ojh = {}; // 工序层面 工件j的第h道工序
+  let Mijh = {}; // 机器层面  工件j的第h道工序在机器i上加工
+  machines.forEach((ele) => {
+    Mijh[ele] = [];
+  });
+  ppc.forEach((ele, index) => {
+    const machineKey = `machine${Jm[ele - 1][hMatrix[index] - 1]}`;
+    const j = ele;
+    const h = hMatrix[index];
+    const duration = T[ele - 1][hMatrix[index] - 1];
 
+    let _res = { _St: 0, _Et: 0 };
 
-    let _res = {_St:0,_Et:0}
-
-    if(Mijh[machineKey].length===0){// 在机器mi是第一道加工工序
-      if(h===1){// 是工件的第一道工序
-        _res._St=0
-        _res._Et=duration
-      }else{// 开始时间 则是上一道工序的结束时间
-        _res._St=Ojh[`O${j}${h-1}`].Et
-        _res._Et=_res._St+duration
+    if (Mijh[machineKey].length === 0) {
+      // 在机器mi是第一道加工工序
+      if (h === 1) {
+        // 是工件的第一道工序
+        _res._St = 0;
+        _res._Et = duration;
+      } else {
+        // 开始时间 则是上一道工序的结束时间
+        _res._St = Ojh[`O${j}${h - 1}`].Et;
+        _res._Et = _res._St + duration;
         // console.log('_res',_res,'Ojh',`O${j}${h}` )
       }
-      Mijh[machineKey].push({process:`O${j}${h}`,St:_res._St,Et:_res._Et})
-    }else{ // 否则找到机器mi上所有间隔空闲时间段[TSi,TEi],注：当前工序前一道工序Oj(h-1)的结束时间后的才是有效区间
-      let idlePeriod=[]
+      Mijh[machineKey].push({
+        process: `O${j}${h}`,
+        St: _res._St,
+        Et: _res._Et,
+      });
+    } else {
+      // 否则找到机器mi上所有间隔空闲时间段[TSi,TEi],注：当前工序前一道工序Oj(h-1)的结束时间后的才是有效区间
+      let idlePeriod = [];
       // 从第一个工序前的空白开始
-      Mijh[machineKey].forEach((ele,index) => {
-        if(index===0){
-          idlePeriod.push([0,ele.St])
-        }else{
-          idlePeriod.push([Mijh[machineKey][index-1].Et,ele.St])
+      Mijh[machineKey].forEach((ele, index) => {
+        if (index === 0) {
+          idlePeriod.push([0, ele.St]);
+        } else {
+          idlePeriod.push([Mijh[machineKey][index - 1].Et, ele.St]);
         }
       });
       // 找到Ojh最早开始加工时间 ta ,能满足工件加工工序的顺序约束：ta=max{Cj(h-1),TSi}
       // 插入条件是:ta + Pijh <= TEi
       // 如果不满足插入条件，则按照时间tb在机器Mi上加工，tb=max{Cj(h-1),LMi} LMi表示当前机器最后一道工序的结束时间
       // 找出第一个可以插入的空闲区间
-      let firstSparePeriod=[]
-      let i = 0
-      let ta=0
+      let firstSparePeriod = [];
+      let i = 0;
+      let ta = 0;
       // console.log('Ojh',Ojh,'`\n O${j}${h-1}`',`O${j}${h-1}`)
-      
-      while (i<idlePeriod.length) {
-        if(h===1){
-          ta = Math.max(0,idlePeriod[i][0])
-        }else{
-          ta = Math.max(Ojh[`O${j}${h-1}`].Et,idlePeriod[i][0])
+
+      while (i < idlePeriod.length) {
+        if (h === 1) {
+          ta = Math.max(0, idlePeriod[i][0]);
+        } else {
+          ta = Math.max(Ojh[`O${j}${h - 1}`].Et, idlePeriod[i][0]);
         }
-        if(idlePeriod[i][1] - ta>=duration){
-          firstSparePeriod.push({fspdex:i,period:idlePeriod[i]})
-          break
+        if (idlePeriod[i][1] - ta >= duration) {
+          firstSparePeriod.push({ fspdex: i, period: idlePeriod[i] });
+          break;
         }
-        i++
+        i++;
       }
-      if(firstSparePeriod.length){
-        Mijh[machineKey].splice(firstSparePeriod[0].fspdex,0,{process:`O${j}${h}`,St:ta,Et:ta+duration})
-        _res._St=ta
-        _res._Et=ta+duration
-      }else{// 如果没有可插入空间则安排到当前机器最后一道工序的结束时间
-        const lastEle=Mijh[machineKey][Mijh[machineKey].length-1]
-        let tb=0
-        if(h-1===0){
-          tb = Math.max(0,lastEle['Et'])
-        }else{
-          tb = Math.max(Ojh[`O${j}${h-1}`].Et,lastEle['Et'])
+      if (firstSparePeriod.length) {
+        Mijh[machineKey].splice(firstSparePeriod[0].fspdex, 0, {
+          process: `O${j}${h}`,
+          St: ta,
+          Et: ta + duration,
+        });
+        _res._St = ta;
+        _res._Et = ta + duration;
+      } else {
+        // 如果没有可插入空间则安排到当前机器最后一道工序的结束时间
+        const lastEle = Mijh[machineKey][Mijh[machineKey].length - 1];
+        let tb = 0;
+        if (h - 1 === 0) {
+          tb = Math.max(0, lastEle["Et"]);
+        } else {
+          tb = Math.max(Ojh[`O${j}${h - 1}`].Et, lastEle["Et"]);
         }
-        Mijh[machineKey].push({process:`O${j}${h}`,St:tb,Et:tb+duration})
-        _res._St=tb
-        _res._Et=tb+duration
+        Mijh[machineKey].push({
+          process: `O${j}${h}`,
+          St: tb,
+          Et: tb + duration,
+        });
+        _res._St = tb;
+        _res._Et = tb + duration;
       }
     }
 
-    Ojh[`O${j}${h}`]={machine:machineKey,duration:duration,St:_res._St,Et:_res._Et}
-
-  })
-  console.log('Ojh',Ojh,'\nMijh',Mijh)
+    Ojh[`O${j}${h}`] = {
+      machine: machineKey,
+      duration: duration,
+      St: _res._St,
+      Et: _res._Et,
+    };
+  });
+  console.log("Ojh", Ojh, "\nMijh", Mijh);
 }
 
-function getMachinePartChromosomes(xJobs,xPopulation,xMachines){
-  let _res=[]
+function getMachinePartChromosomes(xJobs, xPopulation, xMachines) {
+  let _res = [];
   for (let i = 0; i < xPopulation; i++) {
-    _res.push(machineGlobalSelection(xJobs,xMachines));
+    _res.push(machineGlobalSelection(xJobs, xMachines));
   }
-  return _res
+  return _res;
 }
-function getProcessPartChromosomes(xJobs,xPopulation){
- let _res=[]
+function getProcessPartChromosomes(xJobs, xPopulation) {
+  let _res = [];
   for (let i = 0; i < xPopulation; i++) {
     _res.push(processRandomSelection(xJobs));
   }
-  return _res
+  return _res;
 }
 // 机器全局选择
-function machineGlobalSelection(xJobs,xMachines) {
+function machineGlobalSelection(xJobs, xMachines) {
   let _tempArr = [];
-  let xMachineTimeList = Array(xMachines.length).fill(0)
-  const selectionQueue=generateUniqueRandomNumbers(0,9)
-  selectionQueue.forEach(ele => {
-    const gsiItem = selectSingleProcess(xJobs[ele],xMachineTimeList,xMachines);
-    _tempArr.push({order:ele,code:gsiItem})
+  let xMachineTimeList = Array(xMachines.length).fill(0);
+  const selectionQueue = generateUniqueRandomNumbers(0, 9);
+  selectionQueue.forEach((ele) => {
+    const gsiItem = selectSingleProcess(
+      xJobs[ele],
+      xMachineTimeList,
+      xMachines
+    );
+    _tempArr.push({ order: ele, code: gsiItem });
   });
-  _tempArr.sort((a,b)=>{
-    return  a.order- b.order
-  })
-  return _tempArr.map(ele=>ele.code).reduce((a,b)=>a.concat(b),[]);
+  _tempArr.sort((a, b) => {
+    return a.order - b.order;
+  });
+  return _tempArr.map((ele) => ele.code).reduce((a, b) => a.concat(b), []);
 }
-function generateUniqueRandomNumbers(min,max) {
-  const arrayLength=max-min+1
-  const randomNumbers= new Set()
+function generateUniqueRandomNumbers(min, max) {
+  const arrayLength = max - min + 1;
+  const randomNumbers = new Set();
   while (randomNumbers.size < arrayLength) {
-    const randomnumber = min + Math.floor(Math.random()*arrayLength)
-    randomNumbers.add(randomnumber)
+    const randomnumber = min + Math.floor(Math.random() * arrayLength);
+    randomNumbers.add(randomnumber);
   }
-  return [...randomNumbers]
+  return [...randomNumbers];
 }
 // function getRandomInt(max) {
 //   return Math.floor(Math.random() * max);
 // }
-function selectSingleProcess(row,xMachineTimeList,xMachines) {
+function selectSingleProcess(row, xMachineTimeList, xMachines) {
   let _tempArr = [];
   row.forEach((ele) => {
-    let preMachineTimeList=_.cloneDeep(xMachineTimeList)
+    let preMachineTimeList = _.cloneDeep(xMachineTimeList);
     let _tempProIndexs = [];
     xMachines.forEach((eleSon, sondex) => {
       if (ele[eleSon] !== "-") {
@@ -256,7 +366,7 @@ function selectSingleProcess(row,xMachineTimeList,xMachines) {
       }
     });
     // console.log('preMachineTimeList',preMachineTimeList,'xMachineTimeList',xMachineTimeList,'_tempProIndexs',_tempProIndexs,'_minimunValIndex',_minimunValIndex)
-    xMachineTimeList[_minimunValIndex]+=preMachineTimeList[_minimunValIndex]
+    xMachineTimeList[_minimunValIndex] += preMachineTimeList[_minimunValIndex];
     _tempArr.push(_tempProIndexs.indexOf(_minimunValIndex) + 1);
   });
   return _tempArr;
@@ -311,17 +421,17 @@ function getJobs(list) {
       k++;
     }
     jobItems.forEach((ele, index) => {
-      const row = { processName: "J" + j + (index + 1),machineTimeMatrix:[] };// machine-TimeMatrix:[机器,时间]矩阵
+      const row = { processName: "J" + j + (index + 1), machineTimeMatrix: [] }; // machine-TimeMatrix:[机器,时间]矩阵
       ele.forEach((eleSon, sondex) => {
         if (sondex % 2 === 0) {
           row[machines[eleSon - 1]] = ele[sondex + 1];
         }
       });
-      machines.forEach((eleMac,macDex) => {
+      machines.forEach((eleMac, macDex) => {
         if (!(eleMac in row)) {
           row[eleMac] = "-";
-        }else{
-          row['machineTimeMatrix'].push([macDex+1,row[eleMac]])
+        } else {
+          row["machineTimeMatrix"].push([macDex + 1, row[eleMac]]);
         }
       });
       // 合并单元格
@@ -447,10 +557,10 @@ function getProcessNum(list) {
 </script>
 
 <style scoped>
-  .gantt{
-    width: 800px;
-    height: 600px;
-    border: 1px solid #000;
-    border-radius: 5px;
-  }
+.gantt {
+  width: 800px;
+  height: 600px;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
 </style>
