5932db46ed04e9bb0d2fabfb37e675cc2ac25d33
samwise-coder <974049754@qq.com>
2024-05-20 21:52:58 +0800

    feat:适应度函数完成

--- src/components/GA_FJSP.vue
+++ src/components/GA_FJSP.vue
@@ -14,11 +14,13 @@
         ></el-table-column>
       </template>
     </el-table>
+    <div class="gantt" id="gantt"></div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import * as echarts from 'echarts'
+import { ref,onMounted } from "vue";
 import { Mk01 } from "../assets/Mk01";
 import _ from "lodash";
 const tableConfig = ref([]);
@@ -30,10 +32,15 @@ const population = 1; //种群数量
 const machines = getMachines(machineNum); // 所有机器
 tableConfig.value = getTableConfig(machineNum);
 tableData.value = originalDataVisualization(Mk01);
+console.log('tableData.value',tableData.value)
 const jobs = getJobs(Mk01); // 工件数组
 const To = getProcessNum(jobs); // 工序数
 
 console.log('To',To, "\njobs", jobs);
+
+onMounted(()=>{
+  initChart(machines)
+})
 
 let MachinePartChromosomes = getMachinePartChromosomes(jobs,population,machines); // 机器部分染色体集
 let ProcessPartChromosomes = getProcessPartChromosomes(jobs,population,machines); // 工序部分染色体集
@@ -41,7 +48,43 @@ let ProcessPartChromosomes = getProcessPartChromosomes(jobs,population,machines)
 fitness(MachinePartChromosomes[0], ProcessPartChromosomes[0],jobs);
 
 // cross(To);
-
+function initChart(yData) {
+  const gantt =document.getElementById('gantt')
+  const ganttChart = echarts.init(gantt)
+  const option = {
+    grid:{
+      show:false,
+      right:20,
+      left:80,
+      top:20,
+      bottom:20,
+      backgroundColor:'#fff'
+    },
+    legend:{
+      data:[''],
+      align:'auto',
+      top:'bottom'
+    },
+    xAxis:{
+      position:'bottom',
+      type:'time'
+    },
+    yAxis:{
+      data:yData
+    },
+    series:[{
+      type:'bar',
+      stack:'duration',
+      data:['2024-1-1','2024-2-3','2024-4-6','2024-4-20','2024-4-30','2024-5-6','2024-5-16']
+    },
+    {
+      type:'bar',
+      stack:'duration',
+      data:['2023-1-1','2023-2-3','2023-4-6','2023-4-20','2023-4-30','2023-5-6','2023-5-16']
+    },]
+  }
+  ganttChart.setOption(option)
+}
 // 适应度函数 目标：最大完工时间最小化，f=min(max(Cj))
 // 计算每个个体的最大完工时间
 function fitness(mpc, ppc,xJobs) {
@@ -54,7 +97,6 @@ function fitness(mpc, ppc,xJobs) {
     let _machRow=[]
     let _timeRow=[]
     ele.forEach(eleSon=>{
-      console.log('-----',i,mpc[i]-1,eleSon['machineTimeMatrix'])
       _machRow.push(eleSon['machineTimeMatrix'][mpc[i]-1][0] )
       _timeRow.push(eleSon['machineTimeMatrix'][mpc[i]-1][1] )
       i++
@@ -62,7 +104,7 @@ function fitness(mpc, ppc,xJobs) {
     Jm.push(_machRow)
     T.push(_timeRow)
   })
-  console.log("Jm", Jm,'T',T);
+  console.log("Jm", Jm,'\nT',T);
 
   // 工序排序部分解码
   // 转换成 O(jh) 第j个工件的第h道工序
@@ -75,18 +117,85 @@ function fitness(mpc, ppc,xJobs) {
   })
   console.log('hMatrix',hMatrix)
   // 
-  let Ojh={} // 工序层面
-  let Mi = {} // 机器层面
+  let Ojh = {} // 工序层面 工件j的第h道工序
+  let Mijh = {} // 机器层面  工件j的第h道工序在机器i上加工
   machines.forEach(ele=>{
-    Mi[ele]=[]
+    Mijh[ele] = []
   })
   ppc.forEach((ele,index)=>{
-    let _SEObj= getOpStartEnd(Mi,`machine${Jm[ele-1][hMatrix[index]-1]}`,ele,hMatrix[index],Ojh,T[ele-1][hMatrix[index]-1])
-    Ojh[`O${ele}${hMatrix[index]}`]={machine:Jm[ele-1][hMatrix[index]-1],duration:T[ele-1][hMatrix[index]-1],St:_SEObj._St,Et:_SEObj._Et}
+    const machineKey = `machine${Jm[ele-1][hMatrix[index]-1]}`
+    const j = ele
+    const h = hMatrix[index]
+    const duration = T[ele-1][hMatrix[index]-1]
+
+
+    let _res = {_St:0,_Et:0}
+
+    if(Mijh[machineKey].length===0){// 在机器mi是第一道加工工序
+      if(h===1){// 是工件的第一道工序
+        _res._St=0
+        _res._Et=duration
+      }else{// 开始时间 则是上一道工序的结束时间
+        _res._St=Ojh[`O${j}${h-1}`].Et
+        _res._Et=_res._St+duration
+        // console.log('_res',_res,'Ojh',`O${j}${h}` )
+      }
+      Mijh[machineKey].push({process:`O${j}${h}`,St:_res._St,Et:_res._Et})
+    }else{ // 否则找到机器mi上所有间隔空闲时间段[TSi,TEi],注：当前工序前一道工序Oj(h-1)的结束时间后的才是有效区间
+      let idlePeriod=[]
+      // 从第一个工序前的空白开始
+      Mijh[machineKey].forEach((ele,index) => {
+        if(index===0){
+          idlePeriod.push([0,ele.St])
+        }else{
+          idlePeriod.push([Mijh[machineKey][index-1].Et,ele.St])
+        }
+      });
+      // 找到Ojh最早开始加工时间 ta ,能满足工件加工工序的顺序约束：ta=max{Cj(h-1),TSi}
+      // 插入条件是:ta + Pijh <= TEi
+      // 如果不满足插入条件，则按照时间tb在机器Mi上加工，tb=max{Cj(h-1),LMi} LMi表示当前机器最后一道工序的结束时间
+      // 找出第一个可以插入的空闲区间
+      let firstSparePeriod=[]
+      let i = 0
+      let ta=0
+      // console.log('Ojh',Ojh,'`\n O${j}${h-1}`',`O${j}${h-1}`)
+      
+      while (i<idlePeriod.length) {
+        if(h===1){
+          ta = Math.max(0,idlePeriod[i][0])
+        }else{
+          ta = Math.max(Ojh[`O${j}${h-1}`].Et,idlePeriod[i][0])
+        }
+        if(idlePeriod[i][1] - ta>=duration){
+          firstSparePeriod.push({fspdex:i,period:idlePeriod[i]})
+          break
+        }
+        i++
+      }
+      if(firstSparePeriod.length){
+        Mijh[machineKey].splice(firstSparePeriod[0].fspdex,0,{process:`O${j}${h}`,St:ta,Et:ta+duration})
+        _res._St=ta
+        _res._Et=ta+duration
+      }else{// 如果没有可插入空间则安排到当前机器最后一道工序的结束时间
+        const lastEle=Mijh[machineKey][Mijh[machineKey].length-1]
+        let tb=0
+        if(h-1===0){
+          tb = Math.max(0,lastEle['Et'])
+        }else{
+          tb = Math.max(Ojh[`O${j}${h-1}`].Et,lastEle['Et'])
+        }
+        Mijh[machineKey].push({process:`O${j}${h}`,St:tb,Et:tb+duration})
+        _res._St=tb
+        _res._Et=tb+duration
+      }
+    }
+
+    Ojh[`O${j}${h}`]={machine:machineKey,duration:duration,St:_res._St,Et:_res._Et}
 
   })
-  console.log('-------mmm',Ojh,Mi) 
+  console.log('Ojh',Ojh,'\nMijh',Mijh)
 }
+
 function getMachinePartChromosomes(xJobs,xPopulation,xMachines){
   let _res=[]
   for (let i = 0; i < xPopulation; i++) {
@@ -104,21 +213,29 @@ function getProcessPartChromosomes(xJobs,xPopulation){
 // 机器全局选择
 function machineGlobalSelection(xJobs,xMachines) {
   let _tempArr = [];
-  let _jobs = _.cloneDeep(xJobs);
-  let g = _jobs.length;
   let xMachineTimeList = Array(xMachines.length).fill(0)
-  while (g > 0) {
-    const _random = getRandomInt(g);
-    const selectRow = _jobs.splice(_random, 1);
-    const gsiItem = selectSingleProcess(selectRow[0],xMachineTimeList,xMachines);
-    _tempArr = [..._tempArr, ...gsiItem];
-    g--;
+  const selectionQueue=generateUniqueRandomNumbers(0,9)
+  selectionQueue.forEach(ele => {
+    const gsiItem = selectSingleProcess(xJobs[ele],xMachineTimeList,xMachines);
+    _tempArr.push({order:ele,code:gsiItem})
+  });
+  _tempArr.sort((a,b)=>{
+    return  a.order- b.order
+  })
+  return _tempArr.map(ele=>ele.code).reduce((a,b)=>a.concat(b),[]);
+}
+function generateUniqueRandomNumbers(min,max) {
+  const arrayLength=max-min+1
+  const randomNumbers= new Set()
+  while (randomNumbers.size < arrayLength) {
+    const randomnumber = min + Math.floor(Math.random()*arrayLength)
+    randomNumbers.add(randomnumber)
   }
-  return _tempArr;
+  return [...randomNumbers]
 }
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+// function getRandomInt(max) {
+//   return Math.floor(Math.random() * max);
+// }
 function selectSingleProcess(row,xMachineTimeList,xMachines) {
   let _tempArr = [];
   row.forEach((ele) => {
@@ -138,7 +255,7 @@ function selectSingleProcess(row,xMachineTimeList,xMachines) {
         _minimunValIndex = ele;
       }
     });
-    console.log('preMachineTimeList',preMachineTimeList,'xMachineTimeList',xMachineTimeList,'_tempProIndexs',_tempProIndexs,'_minimunValIndex',_minimunValIndex)
+    // console.log('preMachineTimeList',preMachineTimeList,'xMachineTimeList',xMachineTimeList,'_tempProIndexs',_tempProIndexs,'_minimunValIndex',_minimunValIndex)
     xMachineTimeList[_minimunValIndex]+=preMachineTimeList[_minimunValIndex]
     _tempArr.push(_tempProIndexs.indexOf(_minimunValIndex) + 1);
   });
@@ -285,23 +402,7 @@ function objectSpanMethod({ row, columnIndex }) {
     };
   }
 }
-function getOpStartEnd(mi,machineKey,j,h,ojh,duration) {
-  let _res={_St:0,_Et:0}
-  // let _hasOp=
-  if(mi[machineKey].length===0){// 在机器mi是第一道加工工序
-    if(h===1){// 是工件的第一道工序
-      _res._St=0
-      _res._Et=duration
-    }else{// 开始时间 则是上一道工序的结束时间
-      _res._St=ojh[`O${j}${h}`].Et
-      _res._Et=_res._St+duration
-    }
-  }else{ // 否则找到机器mi上所有间隔空闲时间段[TSi,TEi]
-    // let idlePeriod=[]
-  }
 
-  return _res
-}
 // function getOjh(target,list) {}
 // 选择操作
 // function selection(params) {}
@@ -345,5 +446,11 @@ function getProcessNum(list) {
 // }
 </script>
 
-<style>
+<style scoped>
+  .gantt{
+    width: 800px;
+    height: 600px;
+    border: 1px solid #000;
+    border-radius: 5px;
+  }
 </style>
